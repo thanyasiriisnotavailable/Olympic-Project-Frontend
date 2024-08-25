c70b7225648e607fbbdaecaeb66ca70f91d885f9
Sorod Yawa <sorod_yawa@cmu.ac.th>
2024-08-25 22:18:45 +0700

    Create nested route
    when clicked country row link to its details

--- src/services/CountryService.ts
+++ src/services/CountryService.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-
 const apiClient = axios.create({
   baseURL: 'https://my-json-server.typicode.com/chartchai-class/project-1-tiramisu-cake',
   withCredentials: false,
@@ -8,9 +7,11 @@ const apiClient = axios.create({
     'Content-Type': 'application/json'
   }
 })
-
 export default {
   getCountries() {
     return apiClient.get('/countries')
+  },
+  getCountry(id: string) {
+    return apiClient.get('/countries/' + id)
   }
 }
---
+++ src/views/country/DetailView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { toRefs, defineProps } from 'vue'
+import type { Country } from '@/types'
+const props = defineProps<{
+  country: Country
+}>()
+const { country } = toRefs(props)
+</script>
+<template>
+  <div>
+    <h1>This is Detail View</h1>
+  </div>
+</template>
