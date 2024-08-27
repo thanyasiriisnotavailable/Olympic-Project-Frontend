46872961ae70904438bb5c8655b477eca1afe7f2
wanwisasin <Wanwisa_j@cmu.ac.th>
2024-08-28 01:00:21 +0700

    Create country details

--- src/views/country/DetailView.vue
+++ src/views/country/DetailView.vue
@@ -7,7 +7,16 @@ const props = defineProps<{
 const { country } = toRefs(props)
 </script>
 <template>
-  <div>
-    <h1>This is Detail View</h1>
-  </div>
+ <div class="country" v-if="country">
+    <h1>ISO code :{{ country.id }}</h1>
+        <p><b>name:</b> {{ country.name }}</p>
+        <p><b>capital:</b> {{ country.capital }}</p>
+        <p><b>language:</b> {{ country.language }}</p>
+        <img
+            :src="country.flag"
+            :alt="country.name"
+            class="w-auto h-10 border-2 border-black-500"
+          />
+   
+</div>
 </template>
