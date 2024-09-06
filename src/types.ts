10189fbf5ce0a2b4bb21ff7959aab6d1a3f64a48
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-09-07 01:52:59 +0700

    fix the Country  type

--- src/types.ts
+++ src/types.ts
@@ -2,12 +2,10 @@ export interface Country {
   order: number
   id: string
   name: string
-  gold: number
-  silver: number
-  bronze: number
   flag: string
   capital: string
   language: string
+  continent: string
   sports: Sport[]
   comment: CommentState
 }
